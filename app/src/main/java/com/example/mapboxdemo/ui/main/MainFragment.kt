d619f964585ec7daec58cff079a7e1f6e7a1829c
tsemb012 <tsukasa0171015@gmail.com>
2021-12-14 23:50:03 +0900

    MAPBOX-001 例に沿ってマップにマーカーを追加。なぜか表示されないので、一旦保留

--- app/src/main/java/com/example/mapboxdemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mapboxdemo/ui/main/MainFragment.kt
@@ -1,14 +1,25 @@
 package com.example.mapboxdemo.ui.main
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.mapboxdemo.R
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 
 class MainFragment : Fragment() {
 
@@ -28,11 +39,14 @@ class MainFragment : Fragment() {
         mapView = view.findViewById(R.id.mapView) as MapView
         mapView
             .getMapboxMap()//MapBoxのオブジェクト取得
-            .loadStyleUri(
-                Style.MAPBOX_STREETS
-            )
+            .loadStyleUri(Style.MAPBOX_STREETS//スタイルの選定
+            ) { addAnnotationToMap() }//ロード直後に特定のUriからMapを取得している。
+
+
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,22 +62,57 @@ class MainFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        mapView?.onStart()
+        mapView.onStart()
     }
 
     override fun onStop() {
         super.onStop()
-        mapView?.onStop()
+        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
+        mapView.onDestroy()
+    }
+
+    private fun addAnnotationToMap() {// Create an instance of the Annotation API and get the PointAnnotationManager.
+            bitmapFromDrawableRes(requireContext(), R.drawable.ic_launcher_foreground)?.let {
+            val annotationApi = mapView.annotations
+            val pointAnnotationManager = annotationApi.createPointAnnotationManager(mapView) // Set options for the resulting symbol layer.
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions() // Define a geographic coordinate.
+                .withPoint(Point.fromLngLat(18.06, 59.31)) // Specify the bitmap you assigned to the point annotation // The bitmap will be added to map style automatically.
+                .withIconImage(it) // Add the resulting pointAnnotation to the map.
+            pointAnnotationManager.create(pointAnnotationOptions)
+        }
+    }
+
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+// copying drawable object to not manipulate on the same reference
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
     }
 
 }
