f11816540a40a99ce9c0c1ce447046ced7d0e0c2
tsemb012 <tsukasa0171015@gmail.com>
2021-12-14 22:49:29 +0900

    InitialCommit

---
+++ app/src/main/java/com/example/mapboxdemo/ui/main/MainFragment.kt
@@ -0,0 +1,69 @@
+package com.example.mapboxdemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mapboxdemo.R
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var mapView: MapView
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        mapView = view.findViewById(R.id.mapView) as MapView
+        mapView
+            .getMapboxMap()//MapBoxのオブジェクト取得
+            .loadStyleUri(
+                Style.MAPBOX_STREETS
+            )
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+}
